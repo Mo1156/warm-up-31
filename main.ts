bc0a722c70da2c2c9f6e74fb4fa9d48c7e4390ca
Mo1156 <58610705+Mo1156@users.noreply.github.com>
2020-02-05 13:51:24 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let x = 1; 
+let y = 10;
+for (let index = x; index <= y; index++) {
+    game.splash("counting from 1 to 10 :" + index)
+
+}
+let X2 = 11
+let Y2 = 20
+for (let index = X2; index <= Y2; index++) {
+    game.splash("counting from 11 to 20:" + index) 
+}
+let a = 5
+let b = 0
+for (let index = a; index >= b; index--) {
+    game.splash("counting down to 0:" + index)
+}
+let E = 10
+let F = 100
+for (let index = E; index >= F; index-=) {
+    game.splash("counting down to 0:" + index)
+}
